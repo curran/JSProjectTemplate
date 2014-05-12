7c3ddd0811bf1d273cfe2775d228933d40633d06
curran <curran.kelleher@gmail.com>
2014-05-12 11:51:08 -0400

    Updated documentation of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,26 @@ var gulp = require('gulp'),
     stylish = require('jshint-stylish'),
     docco = require('gulp-docco'),
     rename = require('gulp-rename'),
+
+    // Globs for the module and unit test source files.
     theCode = ['src/**/*.js','tests/**/*.js'],
+
+    // The name of the top-level module to create.
     moduleName = 'myModule';
 
+// The default task runs all the others.
+// This executes when you run "gulp"
 gulp.task('default', ['lint', 'build', 'test', 'docs']);
 
+// Checks code quality using JSHint, outputs a stylish report.
 gulp.task('lint', function() {
   gulp.src(theCode)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
 
+// Bundles all the AMD modules together into a single file,
+// outputs non-minified and minified versions of the combined file.
 gulp.task('build', function() {
 
   // Bundle all the AMD modules together into a single file.
@@ -45,11 +54,13 @@ gulp.task('build', function() {
     .pipe(gulp.dest('dist'));
 });
 
+// Runs all the unit tests using Mocha, reports the results.
 gulp.task('test', ['build'], function () {
   gulp.src(['tests/**/*.js'])
     .pipe(mocha({ reporter: 'spec' }));
 });
 
+// Builds documentation using Docco.
 gulp.task('docs', function () {
   gulp.src(theCode).pipe(docco()).pipe(gulp.dest('docs'))
 });
